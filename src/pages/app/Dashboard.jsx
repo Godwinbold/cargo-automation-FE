7b44d0fa19eaf4bbc5db7411b68c4aafdbcfec2d
Samuel3310 <awoluowolabisamuel@gmail.com>
2025-11-18 09:07:18 +0100

    Dashboard component

---
+++ src/pages/app/Dashboard.jsx
@@ -0,0 +1,7 @@
+import Financials from "../../components/app/Financials";
+
+const Dashboard = ({ color, name }) => {
+  return <Financials color={color} name={name} />;
+};
+
+export default Dashboard;
