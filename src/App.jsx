c229dff07bac2b11a3b1af1c8285f01c97655102
Samuel3310 <awoluowolabisamuel@gmail.com>
2025-11-18 09:01:04 +0100

    App main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import AppRoutes from "./Routes";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className='text-2xl font-bold h-screen bg-blue-500' > WELCOME
-      
-    </div>
-  )
+  return <AppRoutes />;
 }
 
-export default App
+export default App;
