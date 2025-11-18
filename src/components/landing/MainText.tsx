0fbaa9af3409c8ba43520752c1a02f115108f4a8
Samuel3310 <awoluowolabisamuel@gmail.com>
2025-11-18 09:06:41 +0100

    Text component for landing page

---
+++ src/components/landing/MainText.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MainText = () => {
+  return (
+    <div>
+      <h1 className="text-2xl md:text-4xl font-bold text-center text-white mt-20">
+        Manage Cargo. Monitor <br /> Performance.
+        <span className="text-[#3DA5E0] font-bold"> Stay in Control</span>
+      </h1>
+      <p className="max-w-[340px] mt-4 text-center text-white mx-auto">
+        One platform. Total visibility. Smarter cargo operations start here.
+      </p>
+    </div>
+  );
+};
+
+export default MainText;
